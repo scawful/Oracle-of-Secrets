6d5854a711f210c39f10c79f15877e23072a2cad
scawful <justin.scofield@outlook.com>
2022-09-30 17:00:30 -0500

    update ram val

--- Util/ram.asm
+++ Util/ram.asm
@@ -5,6 +5,7 @@ MenuScrollLevelV: skip 1
 MenuScrollLevelH: skip 1
 MenuScrollHDirection: skip 2
 MenuItemValueSpoof: skip 2
+ShortSpoof: skip 1 
 MusicNoteValue: skip 2
 OverworldLocationPointer: skip 2
 
