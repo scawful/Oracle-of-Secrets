bdf40379e5ed49de90e08ddb5a6002808a0fbeb4
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-12-15 18:42:11 -0600

    set Link sprite bank before visible

--- Masks/mask_routines.asm
+++ Masks/mask_routines.asm
@@ -7,3 +7,19 @@ Link_CheckNewY_ButtonPress:
 org $078028
 Player_DoSfx2:
 
+
+org $278000
+StartupMasks:
+{
+  ; from vanilla:
+  ; bring the screen into force blank after NMI
+  LDA.b #$80 : STA $13
+
+  ; set links sprite bank 
+  LDA #$10 : STA $BC
+
+  RTL 
+}
+
+org $008827
+JSL StartupMasks
