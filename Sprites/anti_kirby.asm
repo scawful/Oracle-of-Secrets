93ce1dcb5b686a30e2ba68a0e137dfbdc24fab23
scawful <justin.scofield@outlook.com>
2023-09-07 19:41:58 -0400

    Add Anti-Kirby sprite (DW WIP)

---
+++ Sprites/anti_kirby.asm
@@ -0,0 +1,287 @@
+;==============================================================================
+; Sprite Properties
+;==============================================================================
+!SPRID              = $9F ; The sprite ID you are overwriting (HEX)
+!NbrTiles           = 02 ; Number of tiles used in a frame
+!Harmless           = 00  ; 00 = Sprite is Harmful,  01 = Sprite is Harmless
+!HVelocity          = 00  ; Is your sprite going super fast? put 01 if it is
+!Health             = 10  ; Number of Health the sprite have
+!Damage             = 04  ; (08 is a whole heart), 04 is half heart
+!DeathAnimation     = 00  ; 00 = normal death, 01 = no death animation
+!ImperviousAll      = 00  ; 00 = Can be attack, 01 = attack will clink on it
+!SmallShadow        = 01  ; 01 = small shadow, 00 = no shadow
+!Shadow             = 01  ; 00 = don't draw shadow, 01 = draw a shadow 
+!Palette            = 00  ; Unused in this AntiKirby (can be 0 to 7)
+!Hitbox             = 00  ; 00 to 31, can be viewed in sprite draw tool
+!Persist            = 00  ; 01 = your sprite continue to live offscreen
+!Statis             = 00  ; 00 = is sprite is alive?, (kill all enemies room)
+!CollisionLayer     = 01  ; 01 = will check both layer for collision
+!CanFall            = 00  ; 01 sprite can fall in hole, 01 = can't fall
+!DeflectArrow       = 00  ; 01 = deflect arrows
+!WaterSprite        = 00  ; 01 = can only walk shallow water
+!Blockable          = 00  ; 01 = can be blocked by link's shield?
+!Prize              = 00  ; 00-15 = the prize pack the sprite will drop from
+!Sound              = 00  ; 01 = Play different sound when taking damage
+!Interaction        = 00  ; ?? No documentation
+!Statue             = 00  ; 01 = Sprite is statue
+!DeflectProjectiles = 00  ; 01 = Sprite will deflect ALL projectiles
+!ImperviousArrow    = 00  ; 01 = Impervious to arrows
+!ImpervSwordHammer  = 00  ; 01 = Impervious to sword and hammer attacks
+!Boss               = 00  ; 00 = normal sprite, 01 = sprite is a boss
+%Set_Sprite_Properties(Sprite_AntiKirby_Prep, Sprite_AntiKirby_Long);
+
+
+Sprite_AntiKirby_Long:
+PHB : PHK : PLB
+
+JSR Sprite_AntiKirby_Draw ; Call the draw code
+JSL Sprite_CheckActive   ; Check if game is not paused
+BCC .SpriteIsNotActive   ; Skip Main code is sprite is innactive
+
+JSR Sprite_AntiKirby_Main ; Call the main sprite code
+
+.SpriteIsNotActive
+PLB ; Get back the databank we stored previously
+RTL ; Go back to original code
+
+
+Sprite_AntiKirby_Prep:
+PHB : PHK : PLB
+   
+    ; Add more code here to initialize data
+
+PLB
+RTL
+
+
+Sprite_AntiKirby_Main:
+{  
+  
+  LDA.w SprAction, X; Load the SprAction
+  JSL UseImplicitRegIndexedLocalJumpTable; Goto the SprAction we are currently in
+
+  dw AntiKirby_Start
+  dw AntiKirby_WalkRight
+  dw AntiKirby_WalkLeft
+  dw AntiKirby_Hurt
+  dw AntiKirby_Suck
+  dw AntiKirby_Full
+
+
+
+  AntiKirby_Start:
+  {
+    ; %PlayAnimation(0, 0, 10) ; Idle
+    JSL Sprite_DirectionToFacePlayer 
+    TYA : CMP.b #$02 : BCC .WalkRight
+    %GotoAction(2)
+    RTS
+  .WalkRight 
+    %GotoAction(1)
+    RTS
+  }
+
+  AntiKirby_WalkRight:
+  {
+    %PlayAnimation(0, 3, 10) ; Walk Right
+
+    PHX 
+    JSL Sprite_CheckDamageFromPlayerLong
+    %DoDamageToPlayerSameLayerOnContact()
+    PLX 
+
+    %MoveTowardPlayer(10)
+
+    %GotoAction(0)
+
+    RTS
+  }
+
+  AntiKirby_WalkLeft:
+  {
+    %PlayAnimation(4, 7, 10) ; Walk Left
+
+    PHX 
+    JSL Sprite_CheckDamageFromPlayerLong
+    %DoDamageToPlayerSameLayerOnContact()
+    PLX 
+
+    %MoveTowardPlayer(10)
+
+    %GotoAction(0)
+
+    RTS
+  }
+
+  AntiKirby_Hurt:
+  {
+    %PlayAnimation(8, 8, 10) ; Hurt 
+
+    %MoveTowardPlayer(10)
+
+    RTS
+  }
+
+  AntiKirby_Suck:
+  {
+    %PlayAnimation(9, 10, 10) ; Suck
+
+    %MoveTowardPlayer(10)
+
+    RTS
+  }
+
+  AntiKirby_Full:
+  {
+    %PlayAnimation(11, 11, 10) ; Full
+
+    %MoveTowardPlayer(10)
+
+    RTS
+  }
+
+  
+  
+}
+
+
+;==================================================================================================
+; Sprite Draw code
+; --------------------------------------------------------------------------------------------------
+; Draw the tiles on screen with the data provided by the sprite maker editor
+;==================================================================================================
+Sprite_AntiKirby_Draw:
+JSL Sprite_PrepOamCoord
+JSL Sprite_OAM_AllocateDeferToPlayer
+
+LDA $0DC0, X : CLC : ADC $0D90, X : TAY;Animation Frame
+LDA .start_index, Y : STA $06
+
+
+PHX
+LDX .nbr_of_tiles, Y ;amount of tiles -1
+LDY.b #$00
+.nextTile
+
+PHX ; Save current Tile Index?
+    
+TXA : CLC : ADC $06 ; Add Animation Index Offset
+
+PHA ; Keep the value with animation index offset?
+
+ASL A : TAX 
+
+REP #$20
+
+LDA $00 : CLC : ADC .x_offsets, X : STA ($90), Y
+AND.w #$0100 : STA $0E 
+INY
+LDA $02 : CLC : ADC .y_offsets, X : STA ($90), Y
+CLC : ADC #$0010 : CMP.w #$0100
+SEP #$20
+BCC .on_screen_y
+
+LDA.b #$F0 : STA ($90), Y ;Put the sprite out of the way
+STA $0E
+.on_screen_y
+
+PLX ; Pullback Animation Index Offset (without the *2 not 16bit anymore)
+INY
+LDA .chr, X : STA ($90), Y
+INY
+LDA .properties, X : STA ($90), Y
+
+PHY 
+    
+TYA : LSR #2 : TAY
+    
+LDA .sizes, X : ORA $0F : STA ($92), Y ; store size in oam buffer
+    
+PLY : INY
+    
+PLX : DEX : BPL .nextTile
+
+PLX
+
+RTS
+
+
+;==================================================================================================
+; Sprite Draw Generated Data
+; --------------------------------------------------------------------------------------------------
+; This is where the generated Data for the sprite go
+;==================================================================================================
+.start_index
+db $00, $01, $02, $03, $05, $06, $07, $08, $0A, $0B, $0D, $0F, $11
+.nbr_of_tiles
+db 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1
+.x_offsets
+dw 0
+dw 1
+dw 0
+dw 0, 16
+dw 0
+dw -1
+dw 0
+dw 0, -16
+dw 0
+dw 0, 16
+dw 0, 16
+dw -4, 12
+dw -4, 12
+.y_offsets
+dw 0
+dw 0
+dw 0
+dw 0, 0
+dw 0
+dw 0
+dw 0
+dw 0, 0
+dw 0
+dw 0, 0
+dw 0, 0
+dw 0, 0
+dw 0, 0
+.chr
+db $00
+db $02
+db $00
+db $04, $06
+db $00
+db $02
+db $00
+db $04, $06
+db $20
+db $08, $0A
+db $28, $2A
+db $22, $24
+db $22, $24
+.properties
+db $37
+db $37
+db $37
+db $37, $37
+db $77
+db $77
+db $77
+db $77, $77
+db $37
+db $37, $37
+db $37, $37
+db $37, $37
+db $37, $37
+.sizes
+db $02
+db $02
+db $02
+db $02, $02
+db $02
+db $02
+db $02
+db $02, $02
+db $02
+db $02, $02
+db $02, $02
+db $02, $02
+db $02, $02
