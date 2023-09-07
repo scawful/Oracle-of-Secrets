381cccad178d2eca5cb8ea1f277d2b4cfd9a0c17
scawful <justin.scofield@outlook.com>
2023-09-07 19:38:01 -0400

    Fix Sanctuary music transition

---
+++ Dungeons/sanctuary_transition.asm
@@ -0,0 +1,24 @@
+; Disable Code
+; ------------
+org $028BE7
+db $EA, $EA
+
+
+; Load Sanctuary music during Room 02 to 12 transition
+; ----------------------------------------------------
+;org $028BDA
+;db $14
+
+; Room Number (Sanctuary)
+;org $028BDE
+;db $12
+
+
+; Load Hyrule Castle music during Room 12 to 02 transition
+; --------------------------------------------------------
+;org $028BE3
+;db $16
+
+; Room Number (Sewers)
+;org $028BE5
+;db $02
