decc99dc2b727283e9176504f9a65b18592e90e1
scawful <justin.scofield@outlook.com>
2024-01-15 14:05:44 -0500

    Add dungeons.asm to organize dungeon code

---
+++ Dungeons/dungeons.asm
@@ -0,0 +1,37 @@
+incsrc "Dungeons/keyblock.asm"
+print  "End of keyblock.asm               ", pc
+
+incsrc "Dungeons/sanctuary_transition.asm"
+
+incsrc "Dungeons/entrances.asm"
+print  "End of entrances.asm              ", pc
+
+incsrc "Dungeons/mothula.asm"
+print  "End of mothula.asm                ", pc
+
+incsrc "Dungeons/enemy_damage.asm"
+print  "End of enemy_damage.asm           ", pc
+
+; Use of Bank 0x2C begins
+incsrc "Dungeons/Objects/object_handler.asm"
+print  "End of object_handler.asm         ", pc
+
+incsrc "Dungeons/together_warp_tag.asm"
+print  "End of together_warp_tag.asm      ", pc
+
+incsrc "Dungeons/arrghus.asm"
+print  "End of arrghus.asm                ", pc
+
+incsrc "Dungeons/spike_subtype.asm"
+print  "End of spike_subtype.asm          ", pc
+
+incsrc "Dungeons/house_tag.asm"
+print  "End of house_tag.asm              ", pc
+
+incsrc "Dungeons/Lanmola.asm"
+print "End of Lanmola.asm                 ", pc
+
+incsrc "Dungeons/Lanmola_Expanded.asm"
+print "End of Lanmola_Expanded.asm        ", pc
+
+
