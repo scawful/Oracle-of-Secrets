0c41a384cf0883a5dfe69352e3f0748ef8b26bfd
scawful <justin.scofield@outlook.com>
2024-03-09 08:42:42 -0500

    Move Overworld incsrc into overworld.asm

--- Oracle_main.asm
+++ Oracle_main.asm
@@ -58,22 +58,7 @@ namespace Oracle
   print "  -- Overworld --  "
   print ""
   
-  incsrc "Overworld/world_map.asm"
-
-  incsrc "Overworld/pit_damage.asm"
-  print  "End of Overworld/pit_damage.asm   ", pc
-
-  incsrc "Overworld/master_sword.asm"
-  print  "End of master_sword.asm           ", pc
-
-  incsrc "Overworld/maku_tree.asm"
-  print  "End of Overworld/maku_tree.asm    ", pc
-
-  incsrc "Overworld/lost_woods.asm"
-  print  "End of Overworld/lost_woods.asm   ", pc
-  
-  incsrc "Overworld/time_system.asm"
-  print  "End of Overworld/time_system.asm  ", pc
+  incsrc "Overworld/overworld.asm"
 
   print ""
 
---
+++ Overworld/overworld.asm
@@ -0,0 +1,16 @@
+incsrc "Overworld/world_map.asm"
+
+incsrc "Overworld/pit_damage.asm"
+print  "End of Overworld/pit_damage.asm   ", pc
+
+incsrc "Overworld/master_sword.asm"
+print  "End of master_sword.asm           ", pc
+
+incsrc "Overworld/maku_tree.asm"
+print  "End of Overworld/maku_tree.asm    ", pc
+
+incsrc "Overworld/lost_woods.asm"
+print  "End of Overworld/lost_woods.asm   ", pc
+
+incsrc "Overworld/time_system.asm"
+print  "End of Overworld/time_system.asm  ", pc
