b1ff33d50752841cdc7601e55c8e227030cabb06
scawful <justin.scofield@outlook.com>
2024-03-14 20:30:38 -0400

    comment out octoboss temporarily

--- Sprites/all_sprites.asm
+++ Sprites/all_sprites.asm
@@ -107,8 +107,8 @@ print  "End of Lanmola.asm                ", pc
 incsrc "Sprites/Bosses/lanmola_Expanded.asm"
 print  "End of Lanmola_Expanded.asm       ", pc
 
-incsrc "Sprites/Bosses/octoboss.asm"
-print  "End of octoboss.asm               ", pc
+; incsrc "Sprites/Bosses/octoboss.asm"
+; print  "End of octoboss.asm               ", pc
 
 incsrc "Sprites/NPCs/bug_net_kid.asm"
 
