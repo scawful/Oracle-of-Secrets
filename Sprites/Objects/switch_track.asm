4c4f5319297c16ecac26ce74de140478952d87b0
scawful <justin.scofield@outlook.com>
2024-03-28 19:57:08 -0400

    Update switch and track sprite format for code review

--- Sprites/Objects/switch_track.asm
+++ Sprites/Objects/switch_track.asm
@@ -1,3 +1,7 @@
+; =========================================================
+; Sprite Properties
+; =========================================================
+
 !SPRID              = $B0 ; The sprite ID you are overwriting (HEX)
 !NbrTiles           = 04  ; Number of tiles used in a frame
 !Harmless           = 00  ; 00 = Sprite is Harmful,  01 = Sprite is Harmless
@@ -28,6 +32,8 @@
 
 %Set_Sprite_Properties(Sprite_RotatingTrack_Prep, Sprite_RotatingTrack_Long);
 
+; =========================================================
+
 Sprite_RotatingTrack_Long:
 {
   PHB : PHK : PLB
@@ -42,6 +48,8 @@ Sprite_RotatingTrack_Long:
   PLB ; Get back the databank we stored previously
   RTL ; Go back to original code
 }
+
+; =========================================================
 
 ; Modes
 ; 0 = TopLeft -> TopRight
@@ -60,6 +68,8 @@ Sprite_RotatingTrack_Prep:
   PLB
   RTL
 }
+
+; =========================================================
 
 SwitchRam = $37
 
@@ -148,87 +158,89 @@ Sprite_RotatingTrack_Main:
 }
 
 Sprite_RotatingTrack_Draw:
-JSL Sprite_PrepOamCoord
-JSL Sprite_OAM_AllocateDeferToPlayer
+{
+    JSL Sprite_PrepOamCoord
+    JSL Sprite_OAM_AllocateDeferToPlayer
 
-LDA $0DC0, X : CLC : ADC $0D90, X : TAY;Animation Frame
-LDA .start_index, Y : STA $06
-
-
-PHX
-LDX .nbr_of_tiles, Y ;amount of tiles -1
-LDY.b #$00
-.nextTile
-
-PHX ; Save current Tile Index?
-    
-TXA : CLC : ADC $06 ; Add Animation Index Offset
-
-PHA ; Keep the value with animation index offset?
-
-ASL A : TAX 
-
-REP #$20
-
-LDA $00 : CLC : ADC .x_offsets, X : STA ($90), Y
-AND.w #$0100 : STA $0E 
-INY
-LDA $02 : CLC : ADC .y_offsets, X : STA ($90), Y
-CLC : ADC #$0010 : CMP.w #$0100
-SEP #$20
-BCC .on_screen_y
-
-LDA.b #$F0 : STA ($90), Y ;Put the sprite out of the way
-STA $0E
-.on_screen_y
-
-PLX ; Pullback Animation Index Offset (without the *2 not 16bit anymore)
-INY
-LDA .chr, X : STA ($90), Y
-INY
-LDA .properties, X : STA ($90), Y
-
-PHY 
-    
-TYA : LSR #2 : TAY
-    
-LDA .sizes, X : ORA $0F : STA ($92), Y ; store size in oam buffer
-    
-PLY : INY
-    
-PLX : DEX : BPL .nextTile
-
-PLX
-
-RTS
+    LDA $0DC0, X : CLC : ADC $0D90, X : TAY;Animation Frame
+    LDA .start_index, Y : STA $06
 
 
-.start_index
-db $00, $01, $02, $03
-.nbr_of_tiles
-db 0, 0, 0, 0
-.x_offsets
-dw 0
-dw 0
-dw 0
-dw 0
-.y_offsets
-dw 0
-dw 0
-dw 0
-dw 0
-.chr
-db $44
-db $44
-db $44
-db $44
-.properties
-db $7D
-db $3D
-db $FD
-db $BD
-.sizes
-db $02
-db $02
-db $02
-db $02
+    PHX
+    LDX .nbr_of_tiles, Y ;amount of tiles -1
+    LDY.b #$00
+  .nextTile
+
+    PHX ; Save current Tile Index?
+        
+    TXA : CLC : ADC $06 ; Add Animation Index Offset
+
+    PHA ; Keep the value with animation index offset?
+
+    ASL A : TAX 
+
+    REP #$20
+
+    LDA $00 : CLC : ADC .x_offsets, X : STA ($90), Y
+    AND.w #$0100 : STA $0E 
+    INY
+    LDA $02 : CLC : ADC .y_offsets, X : STA ($90), Y
+    CLC : ADC #$0010 : CMP.w #$0100
+    SEP #$20
+    BCC .on_screen_y
+
+    LDA.b #$F0 : STA ($90), Y ;Put the sprite out of the way
+    STA $0E
+  .on_screen_y
+
+    PLX ; Pullback Animation Index Offset (without the *2 not 16bit anymore)
+    INY
+    LDA .chr, X : STA ($90), Y
+    INY
+    LDA .properties, X : STA ($90), Y
+
+    PHY 
+        
+    TYA : LSR #2 : TAY
+        
+    LDA .sizes, X : ORA $0F : STA ($92), Y ; store size in oam buffer
+        
+    PLY : INY
+        
+    PLX : DEX : BPL .nextTile
+
+    PLX
+
+    RTS
+
+
+  .start_index
+    db $00, $01, $02, $03
+  .nbr_of_tiles
+    db 0, 0, 0, 0
+  .x_offsets
+    dw 0
+    dw 0
+    dw 0
+    dw 0
+  .y_offsets
+    dw 0
+    dw 0
+    dw 0
+    dw 0
+  .chr
+    db $44
+    db $44
+    db $44
+    db $44
+  .properties
+    db $7D
+    db $3D
+    db $FD
+    db $BD
+  .sizes
+    db $02
+    db $02
+    db $02
+    db $02
+}
