1cde67578a9c06624d086145b8795e18598d0993
scawful <justin.scofield@outlook.com>
2024-03-30 16:47:18 -0400

    Rename NewBookCode to LinkItem_JumpFeather and format Additionally, changes the jump state to use the ledge hop mode rather than the recoil animation

--- Items/jump_feather.asm
+++ Items/jump_feather.asm
@@ -15,49 +15,48 @@ org $07AFF8 ; LinkItem_BugCatchingNet
 ; =============================================================================
 
 org $2B8000
-NewBookCode:
+LinkItem_JumpFeather:
 {
   JSL $07983A ; Reset swim state
   LDA $46 : BNE .cantuseit
-  LDA #$02 : STA $5D ; state recoil
-  LDA #$01 : STA $4D ; state recoil 2
+    LDA #$02 : STA $5D ; set link state recoil
+    LDA #$02 : STA $4D ; set jumping state (ledge hop)
 
-  ; Length of the jump
-  LDA #$20 
+    ; Length of the jump
+    LDA #$20 : STA $46 
 
-  STA $46 
+    ; Height of the jump
+    LDA #$24 
 
-  ; Height of the jump
-  LDA #$24 
+    ; Set vertical resistance 
+    STA $29
+    STA $02C7
 
-  ; Set vertical resistance 
-  STA $29
-  STA $02C7
-  ; Set Links direction to right(?)
-  LDA #$08 : STA $0340 : STA $67
+    ; Set Links direction to right(?)
+    LDA #$08 : STA $0340 : STA $67
 
-  ; Reset Link movement offsets 
-  STZ $31
-  STZ $30
+    ; Reset Link movement offsets 
+    STZ $31 : STZ $30
 
-  LDA $F4 : AND #$08 : BEQ .noUp
-      LDA #-8 ; Change that -8 if you want higher speed moving up
-      STA $27 ; Vertical recoil
-  .noUp
-  LDA $F4 : AND #$04 : BEQ .noDown
-      LDA #8  ; Change that -8 if you want higher speed moving down
-      STA $27
-  .noDown
-  LDA $F4 : AND #$02 : BEQ .noLeft
-      LDA #-8 ; Change that -8 if you want higher speed moving left
-      STA $28 ; Horizontal recoil
-  .noLeft
-  LDA $F4 : AND #$01 : BEQ .noRight
-      LDA #8  ; Change that 8 if you want higher speed moving right
-      STA $28
-  .noRight
+      LDA $F4 : AND #$08 : BEQ .noUp
+        LDA #-8 ; Change that -8 if you want higher speed moving up
+        STA $27 ; Vertical recoil
+    .noUp
+      LDA $F4 : AND #$04 : BEQ .noDown
+        LDA #8  ; Change that -8 if you want higher speed moving down
+        STA $27
+    .noDown
+      LDA $F4 : AND #$02 : BEQ .noLeft
+        LDA #-8 ; Change that -8 if you want higher speed moving left
+        STA $28 ; Horizontal recoil
+    .noLeft
+      LDA $F4 : AND #$01 : BEQ .noRight
+        LDA #8  ; Change that 8 if you want higher speed moving right
+        STA $28
+    .noRight
+    
   .cantuseit
-  RTL
+    RTL
 }
 
 print  "End of Items/jump_feather.asm     ", pc
