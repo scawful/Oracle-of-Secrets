d20724b1db79daee720465283329e08361c607aa
scawful <justin.scofield@outlook.com>
2024-04-27 19:26:00 -0400

    Adjust king dodongo leg position

--- Sprites/Bosses/king_dodongo.asm
+++ Sprites/Bosses/king_dodongo.asm
@@ -1,4 +1,19 @@
 
+; Adjust leg position to remove gap
+org $1E87F5
+.offset_x
+#_1E87F5: db -22, -22,  22,  22
+
+.offset_y
+#_1E87F9: db -28,   8, -28,   8
+
+.char
+#_1E87FD: db $A2, $A6, $A2, $A6
+
+.prop
+#_1E8801: db $0B, $0B, $4B, $4B
+
+
 org $1E86E5
 KingHelmasaur_CheckBombDamage:
 
