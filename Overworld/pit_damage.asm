452009ad77c5f0bce1be892d2e479822f9f198f9
scawful <justin.scofield@outlook.com>
2024-05-04 13:34:31 -0400

    add final boss pit transition

--- Overworld/pit_damage.asm
+++ Overworld/pit_damage.asm
@@ -1,3 +1,8 @@
-; Pit hole leads to Houlihan room only in area 05
+; Pit hole leads to Houlihan room only in area 4F (final boss)
 org $0794D9
-  LDA $8A : CMP #$05 : BEQ $0794E4
+  LDA $8A : CMP #$4F 
+  BEQ .overworld_pit_transition
+  JSL $01FFD9 ; TakeDamageFromPit
+  RTS
+
+.overworld_pit_transition
