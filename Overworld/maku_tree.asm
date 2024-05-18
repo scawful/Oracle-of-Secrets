085ee2c16ec6d15e790c3bb2ff150ee5c9d74adb
scawful <justin.scofield@outlook.com>
2024-05-17 21:55:15 -0400

    add special area overrides for gfx, sprites, palettes

--- Overworld/maku_tree.asm
+++ Overworld/maku_tree.asm
@@ -14,3 +14,65 @@ org $0EDE29
     ; Exit value for the special area. In Hyrule Magic these are those White markers.
     dw $0180, $0181, $0182, $0189
 }
+
+
+; =========================================================
+; Special Area Properties
+; =========================================================
+
+pushpc
+
+; =========================================================
+; Exit 180 to Master Sword Area
+; =========================================================
+
+; Sprite GFX
+org $02E811 : db $0C ; PC Address $016811
+
+; Background GFX
+org $02E821 : db $2F ; PC Address $016821
+
+; Palette
+org $02E831 : db $0A ; PC Address $016831
+
+; Sprite Palette
+org $02E841 : db $01 ; PC Address $016841
+
+; =========================================================
+; Exit 181 to Bridge Area
+; =========================================================
+
+; Sprite GFX
+org $02E812 : db $13 ; PC Address $016812
+
+; Background GFX
+org $02E822 : db $2F ; PC Address $016822
+
+; Palette
+org $02E832 : db $0A ; PC Address $016832
+
+; Sprite Palette
+org $02E842 : db $08 ; PC Address $016842
+
+; =========================================================
+; Exit 182 to Zora's Waterfall
+; =========================================================
+
+; Sprite GFX
+org $02E813 : db $0E ; PC Address $016813
+
+; Background GFX
+org $02E823 : db $2F ; PC Address $016823
+
+; Palette
+org $02E833 : db $0A ; PC Address $016833
+
+; Sprite Palette
+org $02E843 : db $03 ; PC Address $016843
+
+; Disable Zora's Waterfall SFX
+org $02C444 : db $55 ; PC Address $014444
+
+pullpc
+
+; =========================================================
