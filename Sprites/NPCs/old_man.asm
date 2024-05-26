290dee574b2fa1a8f496a0604e7c460609172e6e
scawful <justin.scofield@outlook.com>
2024-05-26 09:38:00 -0400

    check for goldstar instead of mirror for mountain spawn point option

--- Sprites/NPCs/old_man.asm
+++ Sprites/NPCs/old_man.asm
@@ -20,3 +20,9 @@ org $1BBD3C
 ; Underworld_LoadEntrance
 org $02D98B
   CMP.w #$02
+
+; Module05_LoadFile
+; Check for goldstar instead of mirror for mountain spawn option
+org $0281E2
+  LDA.l $7EF342
+  CMP.b #$02
