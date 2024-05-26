b6797a8d4e71647fee40eb34a629d5ae76e76512
scawful <justin.scofield@outlook.com>
2024-05-26 08:44:50 -0400

    fix impa telepathic dialogue opening castle bridge overlay

--- Overworld/overworld.asm
+++ Overworld/overworld.asm
@@ -1,3 +1,12 @@
+; Overworld.asm
+
+; Module15_0C
+; Change overlay that Impa activates after intro
+org $029E2E
+#_029E2E: LDA.l $7EF2A3
+#_029E32: ORA.b #$20
+#_029E34: STA.l $7EF2A3
+
 incsrc "Overworld/world_map.asm"
 
 incsrc "Overworld/pit_damage.asm"
