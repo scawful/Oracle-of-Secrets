823a76f15122de99f93aabe14ccef89d329e40e4
scawful <justin.scofield@outlook.com>
2024-05-26 09:40:06 -0400

    Change overlay that Impa activates after DW dialogue

--- Sprites/NPCs/impa.asm
+++ Sprites/NPCs/impa.asm
@@ -3,7 +3,13 @@ pushpc
 org $05EBCF
   LDA $7EF359 : CMP.b #$04
 
-org $05ED63
-  LDA.b #$03
+; Module15_0C
+; Change overlay that Impa activates after intro
+org $029E2E
+#_029E2E: LDA.l $7EF2A3
+#_029E32: ORA.b #$20
+#_029E34: STA.l $7EF2A3
+
+
 
 pullpc
