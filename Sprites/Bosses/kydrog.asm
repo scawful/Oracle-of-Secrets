c9e3a8bc0fac5ef50b9989a5f37d1982f1e87aa6
scawful <justin.scofield@outlook.com>
2024-06-11 22:14:38 -0400

    remove bad reference to song in dungeon bank from overworld cutscene, likely cause of snes9x core crashes

--- Sprites/Bosses/kydrog.asm
+++ Sprites/Bosses/kydrog.asm
@@ -95,7 +95,6 @@ Sprite_Kydrog_Main:
   Kydrog_AttractPlayer:
   {
     LDA.w SprTimerA, X : BNE +
-      LDA.b #$1D : STA $012C
       LDA #$00 : STA $7EF303
       %ShowUnconditionalMessage($21)
       %GotoAction(2)
