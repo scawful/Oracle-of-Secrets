b637195ffe640bf48aa36f0dc538cd1c0fcf0d86
scawful <justin.scofield@outlook.com>
2024-06-18 10:58:34 -0400

    add link control handler and items to tables reference

--- Core/tables.asm
+++ Core/tables.asm
@@ -289,3 +289,65 @@ dw Module07_17_PressurePlate                    ; 0x17
 dw Module07_18_RescuedMaiden                    ; 0x18
 dw Module07_19_MirrorFade                       ; 0x19
 dw Module07_1A_RoomDraw_OpenTriforceDoor_bounce ; 0x1A
+
+
+Link_ControlHandler
+
+  dw LinkState_Default                  ; 0x00
+  dw LinkState_Pits                     ; 0x01
+  dw LinkState_Recoil                   ; 0x02
+  dw LinkState_SpinAttack               ; 0x03
+  dw LinkState_Swimming                 ; 0x04
+  dw LinkState_OnIce                    ; 0x05
+  dw LinkState_Recoil                   ; 0x06
+  dw LinkState_Zapped                   ; 0x07
+  dw LinkState_UsingEther               ; 0x08
+  dw LinkState_UsingBombos              ; 0x09
+  dw LinkState_UsingQuake               ; 0x0A
+  dw LinkState_HoppingSouthOW           ; 0x0B
+  dw LinkState_HoppingHorizontallyOW    ; 0x0C
+  dw LinkState_HoppingDiagonallyUpOW    ; 0x0D
+  dw LinkState_HoppingDiagonallyDownOW  ; 0x0E
+  dw LinkState_0F                       ; 0x0F
+  dw LinkState_0F                       ; 0x10
+  dw LinkState_Dashing                  ; 0x11
+  dw LinkState_ExitingDash              ; 0x12
+  dw LinkState_Hookshotting             ; 0x13
+  dw LinkState_CrossingWorlds           ; 0x14
+  dw LinkState_ShowingOffItem           ; 0x15
+  dw LinkState_Sleeping                 ; 0x16
+  dw LinkState_Bunny                    ; 0x17
+  dw LinkState_HoldingBigRock           ; 0x18
+  dw LinkState_ReceivingEther           ; 0x19
+  dw LinkState_ReceivingBombos          ; 0x1A
+  dw LinkState_ReadingDesertTablet      ; 0x1B
+  dw LinkState_TemporaryBunny           ; 0x1C
+  dw LinkState_TreePull                 ; 0x1D
+  dw LinkState_SpinAttack               ; 0x1E
+
+Link_HandleYItem
+
+  dw LinkItem_Bombs
+  dw LinkItem_Boomerang
+  dw LinkItem_Bow
+  dw LinkItem_Hammer
+
+  dw LinkItem_Rod
+  dw LinkItem_Rod
+  dw LinkItem_Net
+  dw LinkItem_ShovelAndFlute
+
+  dw LinkItem_Lamp
+  dw LinkItem_Powder
+  dw LinkItem_Bottle
+  dw LinkItem_Book
+
+  dw LinkItem_CaneOfByrna
+  dw LinkItem_Hookshot
+  dw LinkItem_Bombos
+  dw LinkItem_Ether
+
+  dw LinkItem_Quake
+  dw LinkItem_CaneOfSomaria
+  dw LinkItem_Cape
+  dw LinkItem_Mirror
