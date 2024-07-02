3919a0dc7b28bcd78c6b2f670168b967d0fd2566
scawful <justin.scofield@outlook.com>
2024-07-01 23:14:54 -0400

    cleanup new sprite table

--- Sprites/ZSpriteLib/sprite_new_table.asm
+++ Sprites/ZSpriteLib/sprite_new_table.asm
@@ -11,69 +11,76 @@ RTS
 pullpc
 
 
-
 SpriteActiveExp_MainLong:
 {
-PHB : PHK : PLB
+  PHB : PHK : PLB
 
-JSL NewSprTable
+  JSL NewSprTable
 
-PLB
+  PLB
 
-RTL
+  RTL
 }
 
 NewSprTable:
-LDA $0E20, X ; Load Sprite ID
-REP #$30
-AND.w #$00FF
-STA $06
-ASL A ; *2
-CLC : ADC $06 ; *3
-TAY
+{
+  LDA $0E20, X ; Load Sprite ID
+  REP #$30
+  AND.w #$00FF
+  STA $06
+  ASL A ; *2
+  CLC : ADC $06 ; *3
+  TAY
 
-LDA NewSprRoutinesLong, Y ; Load sprite Address
-STA $06
-SEP #$20 ; Previously SEP #$30 -_- (that's fine for sprites below ~0x40 over that it will crash)
-LDA NewSprRoutinesLong+2, Y 
-STA $08
-SEP #$30
-JMP [$0006]
+  LDA NewSprRoutinesLong, Y ; Load sprite Address
+  STA $06
+  SEP #$20 ; Previously SEP #$30 -_- (that's fine for sprites below ~0x40 over that it will crash)
+  LDA NewSprRoutinesLong+2, Y 
+  STA $08
+  SEP #$30
+  JMP [$0006]
 
-;do a JML and sprite will RTL back to previous code
+  ;do a JML and sprite will RTL back to previous code
+}
 
 Sprite_PrepExp_Long:
 {
-PHB : PHK : PLB
+  PHB : PHK : PLB
 
-JSL NewSprPrepTable
+  JSL NewSprPrepTable
 
-PLB
+  PLB
 
-RTL
+  RTL
 }
 
 NewSprPrepTable:
-LDA $0E20, X ; Load Sprite ID
-REP #$30 
-AND.w #$00FF
-STA $06
-ASL A ; *2
-CLC : ADC $06 ; *3
-TAY
+{
+  LDA $0E20, X ; Load Sprite ID
+  REP #$30 
+  AND.w #$00FF
+  STA $06
+  ASL A ; *2
+  CLC : ADC $06 ; *3
+  TAY
 
-LDA NewSprPrepRoutinesLong, Y ; Load sprite Address
-STA $06
-SEP #$20 ; Previously SEP #$30 -_- (that's fine for sprites below ~0x40 over that it will crash)
-LDA NewSprPrepRoutinesLong+2, Y 
-STA $08
-SEP #$30
-JMP [$0006]
+  LDA NewSprPrepRoutinesLong, Y ; Load sprite Address
+  STA $06
+  SEP #$20 ; Previously SEP #$30 -_- (that's fine for sprites below ~0x40 over that it will crash)
+  LDA NewSprPrepRoutinesLong+2, Y 
+  STA $08
+  SEP #$30
+  JMP [$0006]
+}
 
 NewSprRoutinesLong:
-fillbyte $00
-fill $2FD
+{
+  fillbyte $00
+  fill $2FD
+}
 
 NewSprPrepRoutinesLong:
-fillbyte $00
-fill $2FD
+{
+  fillbyte $00
+  fill $2FD
+}
