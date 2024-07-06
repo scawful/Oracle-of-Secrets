9045abf9193b2e376c3320acebeb3a5e2664a7a4
scawful <justin.scofield@outlook.com>
2024-07-06 17:07:31 -0400

    cleanup link ram

--- Sprites/ZSpriteLib/sprites.asm
+++ Sprites/ZSpriteLib/sprites.asm
@@ -509,11 +509,26 @@ LinkYH       = $21 ; High position Y of link
 LinkX        = $22 ; Position X of link
 LinkXH       = $23 ; High position X of link
 
-LinkPushDir  = $26 ; ----UDLR [U Up][D Down][L Left][R Right] Direction link is pushing against 
-LinkFaceDir  = $2F ; Direction link is facing 00:Up, 02:Down, 04:Left, 06:Right
-LinkLastDir  = $66 ; Last direction link moved towards 00:Up, 01:Down, 02:Left, 03:Right
-LinkMoveDir  = $67 ; ----UDLR [U Up][D Down][L Left][R Right] direction link is "walking towards"
-LinkMoveInfo = $6A ; 0: Not moving, 1: Moving but NOT diagonally, 2: Moving diagonally
+; ----UDLR 
+;  [U Up][D Down][L Left][R Right] 
+;  Direction link is pushing against 
+LinkPushDir  = $26
+
+; Direction link is facing 
+; 00:Up, 02:Down, 04:Left, 06:Right
+LinkFaceDir  = $2F
+
+; Last direction link moved towards 
+; 00:Up, 01:Down, 02:Left, 03:Right
+LinkLastDir  = $66
+
+; ----UDLR 
+;  [U Up][D Down][L Left][R Right] 
+;  direction link is "walking towards"
+LinkMoveDir  = $67
+
+; 0: Not moving, 1: Moving cardinal, 2: Moving diagonally
+LinkMoveInfo = $6A
 
 LinkRecoilY  = $27 ; Recoiling speed Y of link
 LinkRecoilX  = $28 ; Recoiling speed X of link
@@ -521,27 +536,102 @@ LinkRecoilX  = $28 ; Recoiling speed X of link
 LinkVisible  = $4B ; if set to 0x0C link will be invisible
 LinkBunnyGfx = $56 ; if set to 1 link will be bunny, otherwise link
 
-LinkSpeed    = $57 ; 0x00: normal speed, 0x01-0x0F: slow,�> 0x10:fast
-LinkSpeedTbl = $5E ; 0x00: normal speed, 0x02: walking on stair speed, 0x10: dashing speed
-LinkFalling  = $5B ; if is set to 0x02 or 0x03 link is falling
+; 0x00: normal speed, 0x01-0x0F: slow,�> 0x10:fast
+LinkSpeed    = $57
 
-LinkState    = $5D ; See documentation for that address (0x00 = normal ground state, 0x01 falling, 0x02 recoil, 0x03 spin attack) and many more
-LinkDoorway  = $6C ; 0: Link is not in a doorway, 1: is in a vertical doorway, 2: is in horizontal doorway
+; 0x00: normal speed, 0x02: walking on stair speed, 0x10: dashing speed
+LinkSpeedTbl = $5E
 
+; if is set to 0x02 or 0x03 link is falling 
+LinkFalling  = $5B
 
-LinkGrabGfx  = $02DA ; 0: Nothing, 1: a hand in the air, 2: 2 hands in the air (like getting triforce)
-LinkPoofGfx  = $02E1 ; if not 0 add a poof gfx on link
-LinkBunTimer = $02E2 ; Bunny timer for link how many time you will stay in bunny before transforming back
-LinkMenuMove = $02E4 ; if not 0 prevent link from moving and opening the menu
-LinkDamage   = $037B ; if not 0 prevent link from getting any damages from sprites
+; LinkState_Default                  : 0x00
+; LinkState_Pits                     : 0x01
+; LinkState_Recoil                   : 0x02
+; LinkState_SpinAttack               : 0x03
+; LinkState_Swimming                 : 0x04
+; LinkState_OnIce                    : 0x05
+; LinkState_Recoil                   : 0x06
+; LinkState_Zapped                   : 0x07
+; LinkState_UsingEther               : 0x08
+; LinkState_UsingBombos              : 0x09
+; LinkState_UsingQuake               : 0x0A
+; LinkState_HoppingSouthOW           : 0x0B
+; LinkState_HoppingHorizontallyOW    : 0x0C
+; LinkState_HoppingDiagonallyUpOW    : 0x0D
+; LinkState_HoppingDiagonallyDownOW  : 0x0E
+; LinkState_0F                       : 0x0F
+; LinkState_0F                       : 0x10
+; LinkState_Dashing                  : 0x11
+; LinkState_ExitingDash              : 0x12
+; LinkState_Hookshotting             : 0x13
+; LinkState_CrossingWorlds           : 0x14
+; LinkState_ShowingOffItem           : 0x15
+; LinkState_Sleeping                 : 0x16
+; LinkState_Bunny                    : 0x17
+; LinkState_HoldingBigRock           : 0x18
+; LinkState_ReceivingEther           : 0x19
+; LinkState_ReceivingBombos          : 0x1A
+; LinkState_ReadingDesertTablet      : 0x1B
+; LinkState_TemporaryBunny           : 0x1C
+; LinkState_TreePull                 : 0x1D
+; LinkState_SpinAttack               : 0x1E
+LinkState    = $5D
 
-LinkColChest = $02E5 ; ----CCCC [C Touching chest id]
-LinkSomaria  = $02F5 ; 0: Not on somaria platform, 2: On somaria platform
-LinkItemUse  = $0301 ; BP-AETHR [B Boomerang][P Powder][A Bow&Arrows][E UnusedItem][T UnusedItem][H Hammer][R Rods]
+; 0: Link is not in a doorway
+; 1: is in a vertical doorway
+; 2: is in horizontal doorway
+LinkDoorway  = $6C 
+
+; 0: Nothing
+; 1: a hand in the air
+; 2: 2 hands in the air (like getting triforce)
+LinkGrabGfx  = $02DA 
+
+; if not 0 add a poof gfx on link
+LinkPoofGfx  = $02E1 
+
+; Bunny timer for link before transforming back
+LinkBunTimer = $02E2 
+
+; if not 0 prevent link from moving and opening the menu
+LinkMenuMove = $02E4
+
+; if not 0 prevent link from getting any damages from sprites
+LinkDamage   = $037B
+
+; ----CCCC 
+;  [C Touching chest id]
+LinkColChest = $02E5
+
+; 0: Not on somaria platform, 2: On somaria platform
+LinkSomaria  = $02F5
+
+; BP-AETHR 
+;   [B Boomerang][P Powder]
+;   [A Bow&Arrows][E UnusedItem]
+;   [T UnusedItem][H Hammer][R Rods]
+LinkItemUse  = $0301
+
 LinkItemY    = $0303 ; Currently equipped item on the Y button
-LinkCarrying = $0308 ; 0: Nothing, 1:Picking up something, 2: Throwing something
 
-LinkAnim     = $037A ; 0: Normal, 1: Shovel, 2: Praying, 4:Hookshot, 8:Somaria, 10: Bug net, 20: Read book, 40: Tree pull
+; 0: Nothing, 1:Picking up something, 2: Throwing something
+LinkCarrying = $0308
+
+; .... ..tl
+;   t - tossing object
+;   l - lifting object
+LinkCarryOrToss = $0309
+
+; 0: Normal
+; 1: Shovel
+; 2: Praying
+; 4: Hookshot
+; 8: Somaria
+; 10: Bug net
+; 20: Read book
+; 40: Tree pull
+LinkAnim     = $037A
 
 LinkWallCheat = $037F ; If non zero can walk through walls
 
