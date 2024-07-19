7c6fafbdd0f916a9523587cc40ef31432abaeaf6
scawful <justin.scofield@outlook.com>
2024-07-18 21:41:26 -0400

    add Link_FallIntoDungeon, takes X to index entrance IDs

--- Dreams/all_dreams.asm
+++ Dreams/all_dreams.asm
@@ -33,6 +33,8 @@ Link_HandleDreams:
   Dream_MushroomGrotto:
   {
     LDA.l DREAMS : ORA.b #%00000001 : STA.l DREAMS
+    LDX.b #$00
+    JSR Link_FallIntoDungeon
     RTS
   }
 
@@ -71,6 +73,21 @@ Link_HandleDreams:
     LDA.l DREAMS : ORA.b #%01000000 : STA.l DREAMS
     RTS
   }
+}
+
+; Takes X as argument for the entrance ID 
+Link_FallIntoDungeon:
+{
+  LDA.w .entrance, X
+  STA.w $010E
+  STZ.w $010F
+
+  LDA.b #$11 : STA.b $10
+  STZ.b $11 : STZ.b $B0
+
+  RTS
+.entrance
+  db $7B, $01
 }
 
 print "End of all_dreams.asm             ", pc
