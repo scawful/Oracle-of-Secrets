cc202bbb3ed29975e973d8ee4a3475bb3a2d9dcd
scawful <justin.scofield@outlook.com>
2024-07-18 21:11:45 -0400

    add Link_EnterDream in Dreams/all_dreams.asm

---
+++ Dreams/all_dreams.asm
@@ -0,0 +1,62 @@
+; Contains all the dreams in the game
+; Each dream is a separate subroutine
+
+; To enter a dream, Link will see the Maku tree
+; after getting an essence from a dungeon.
+; We will put the player into LinkState_Sleeping
+
+Link_EnterDream:
+{
+  LDA.w CurrentDream
+  JSL JumpTableLocal
+
+  dw Dream_MushroomGrotto
+  dw Dream_TailPalace
+  dw Dream_KalyxoCastle
+  dw Dream_ZoraTemple
+  dw Dream_GlaciaEstate
+  dw Dream_GoronMines
+  dw Dream_DragonShip
+
+  Dream_MushroomGrotto:
+  {
+    LDA.l DREAMS : ORA.b #%00000001 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_TailPalace:
+  {
+    LDA.l DREAMS : ORA.b #%00000010 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_KalyxoCastle:
+  {
+    LDA.l DREAMS : ORA.b #%00000100 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_ZoraTemple:
+  {
+    LDA.l DREAMS : ORA.b #%00001000 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_GlaciaEstate:
+  {
+    LDA.l DREAMS : ORA.b #%00010000 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_GoronMines:
+  {
+    LDA.l DREAMS : ORA.b #%00100000 : STA.l DREAMS
+    RTS
+  }
+
+  Dream_DragonShip:
+  {
+    LDA.l DREAMS : ORA.b #%01000000 : STA.l DREAMS
+    RTS
+  }
+}
