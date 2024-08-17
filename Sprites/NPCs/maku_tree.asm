c33cdc87ca6d134d397e039c1c69c8d73d402b16
scawful <justin.scofield@outlook.com>
2024-08-17 18:36:37 -0400

    fix maku tree hitbox on subsequent visits

--- Sprites/NPCs/maku_tree.asm
+++ Sprites/NPCs/maku_tree.asm
@@ -13,7 +13,7 @@
 !SmallShadow        = 00  ; 01 = small shadow, 00 = no shadow
 !Shadow             = 00  ; 00 = don't draw shadow, 01 = draw a shadow 
 !Palette            = 0   ; Unused in this template (can be 0 to 7)
-!Hitbox             = $1D ; 00 to 31, can be viewed in sprite draw tool
+!Hitbox             = $0D ; 00 to 31, can be viewed in sprite draw tool
 !Persist            = 00  ; 01 = your sprite continue to live offscreen
 !Statis             = 00  ; 00 = is sprite is alive?, (kill all enemies room)
 !CollisionLayer     = 00  ; 01 = will check both layer for collision
@@ -38,7 +38,6 @@ Sprite_MakuTree_Long:
 {
     PHB : PHK : PLB
 
-    JSR Sprite_MakuTree_Draw ; Call the draw code
     JSL Sprite_CheckActive   ; Check if game is not paused
     BCC .SpriteIsNotActive   ; Skip Main code is sprite is innactive
 
@@ -169,73 +168,4 @@ Sprite_MakuTree_Main:
 
 }
 
-; =========================================================
-
-Sprite_MakuTree_Draw:
-{
-  JSL Sprite_PrepOamCoord
-  JSL Sprite_OAM_AllocateDeferToPlayer
-
-  LDA $0DC0, X : CLC : ADC $0D90, X : TAY;Animation Frame
-  LDA .start_index, Y : STA $06
-
-
-  PHX
-  LDX   .nbr_of_tiles, Y ;amount of tiles -1
-  LDY.b #$00
-  .nextTile
-
-  PHX ; Save current Tile Index?
-      
-  TXA : CLC : ADC $06 ; Add Animation Index Offset
-
-  PHA ; Keep the value with animation index offset?
-
-  ASL A : TAX
-
-  REP #$20
-
-  LDA $00 : CLC : ADC .x_offsets, X : STA ($90), Y
-  AND.w #$0100 : STA $0E
-  INY
-  LDA $02 : CLC : ADC .y_offsets, X : STA ($90), Y
-  CLC   : ADC #$0010 : CMP.w #$0100
-  SEP   #$20
-  BCC   .on_screen_y
-
-  LDA.b #$F0 : STA ($90), Y ;Put the sprite out of the way
-  STA   $0E
-  .on_screen_y
-
-  PLX ; Pullback Animation Index Offset (without the *2 not 16bit anymore)
-  INY
-  LDA .chr, X : STA ($90), Y
-  INY
-  LDA .properties, X : STA ($90), Y
-
-  PHY 
-      
-  TYA : LSR #2 : TAY
-      
-  LDA .sizes, X : ORA $0F : STA ($92), Y ; store size in oam buffer
-      
-  PLY : INY
-      
-  PLX : DEX : BPL .nextTile
-
-  PLX
-
-  RTS
-
-; =========================================================
-
-.start_index
-.nbr_of_tiles
-.x_offsets
-.y_offsets
-.chr
-.properties
-.sizes
-
-}
 
