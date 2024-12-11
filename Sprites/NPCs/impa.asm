41e3fd67ed7dc1ca4c5f05b2042640ebbe949911
scawful <justin.scofield@outlook.com>
2024-12-11 09:08:05 -0500

    Cleanup impa npc asm

--- Sprites/NPCs/impa.asm
+++ Sprites/NPCs/impa.asm
@@ -18,36 +18,26 @@ Impa_SetSpawnPointFlag:
 pushpc
 
 ; Zelda_AtSanctuary
-org $05EE46
-  JSL Impa_SetSpawnPointFlag
+org $05EE46 : JSL Impa_SetSpawnPointFlag
 
 ; TODO: Figure out what to do with this
-org $05EBCF
-  LDA $7EF359 : CMP.b #$05
+org $05EBCF : LDA $7EF359 : CMP.b #$05
 
 ; Module15_0C
 ; Change overlay that Impa activates after intro
-org $029E2E
-#_029E2E: LDA.l $7EF2A3
-#_029E32: ORA.b #$20
-#_029E34: STA.l $7EF2A3
+org $029E2E : LDA.l $7EF2A3 : ORA.b #$20 : STA.l $7EF2A3
 
 ; Prevent Impa from setting spawn point
 org $05ED43
 Zelda_BecomeFollower:
 STZ.w $02E4
-#_05ED46: LDA.b #$00
-#_05ED48: STA.l $7EF3C8
+LDA.b #$00 : STA.l $7EF3C8
+
+org $05ED63 : NOP #5
 
 ; Prevent Impa from changing the song
 org $05ED10
-NOP #5
-
-; LDA.b #$19 ; SONG 19
-; STA.w $012C
-org $05ED63
-NOP #5
-; #_05ED63: LDA.b #$10 ; SONG 10
-; #_05ED65: STA.w $012C
+Zelda_ApproachHero:
+  NOP #5
 
 pullpc
