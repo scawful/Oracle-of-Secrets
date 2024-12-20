ac34c96f4f8356894b52c962878bd7759ce9362f
scawful <justin.scofield@outlook.com>
2024-12-20 17:21:17 -0500

    Reduce PolsVoice OAM allocation

--- Sprites/Enemies/pols_voice.asm
+++ Sprites/Enemies/pols_voice.asm
@@ -3,7 +3,7 @@
 ; =========================================================
 
 !SPRID              = Sprite_PolsVoice
-!NbrTiles           = 03  ; Number of tiles used in a frame
+!NbrTiles           = 02  ; Number of tiles used in a frame
 !Harmless           = 00  ; 00 = Sprite is Harmful,  01 = Sprite is Harmless
 !HVelocity          = 00  ; Is your sprite going super fast? put 01 if it is
 !Health             = 10  ; Number of Health the sprite have
