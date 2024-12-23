3711641e5dd93d954853ee688340f3e808de331c
scawful <justin.scofield@outlook.com>
2024-12-22 20:43:30 -0500

    Add structs.asm with Sprite struct

---
+++ Core/structs.asm
@@ -0,0 +1,63 @@
+
+struct Sprite $7E0BA0
+{
+  .BulletProof: skip 16
+  .AncillaId:   skip 16
+  .Slot:        skip 16
+  .OwDeath:     skip 32
+  .Prize:       skip 16
+  ; 7E0BF0
+  ; 7E0C9A
+  .OwScreen:    skip 16
+  .Deflect:     skip 16
+  .ForceDrop:   skip 16
+  .OverlordRoom: skip 8
+  .BumpDamage: skip 16
+  .CurrDamage: skip 16
+  ; 0CF2
+  ; 0CFF
+  .yl: skip 16
+  .xl: skip 16
+  .yh: skip 16
+  .xh: skip 16
+  .vy: skip 16
+  .vx: skip 16
+  .sub_vy: skip 16
+  .sub_vx: skip 16
+  .action: skip 16
+  .frame: skip 16
+  .misc_a: skip 16
+  .misc_b: skip 16
+  .gfx: skip 16
+  .state: skip 16
+  .misc_c: skip 16
+  .TimerA: skip 16
+  .TimerB: skip 16
+  .TimerC: skip 16
+  .type: skip 16
+  .subtype: skip 16
+  .nbr_oam: skip 16
+  .hp: skip 16
+  .props: skip 16
+  .collide: skip 16
+  .delay: skip 16
+  .misc_d: skip 16
+  .recoil: skip 16
+  .misc_e: skip 16
+  .misc_f: skip 16
+  .misc_g: skip 16
+  .timer_d: skip 16
+  .death_timer: skip 16
+  .pause: skip 16
+  .timer_e: skip 16
+  .floor: skip 16
+  .recoil_y: skip 16
+  .recoil_x: skip 16
+  .oam_prop: skip 16
+  .collision_prop: skip 16
+  .z: skip 16
+  .vz: skip 16
+  .sub_vz: skip 16
+}
+endstruct
+
