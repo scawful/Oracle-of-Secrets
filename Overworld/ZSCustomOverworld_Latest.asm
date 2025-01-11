1fa904a10904fc1856cea7e3099b18170eb12e0a
scawful <justin.scofield@outlook.com>
2025-01-10 19:40:46 -0500

    Change ZSCustomOverworld default overlay to rain for song of storms

--- Overworld/ZSCustomOverworld_Latest.asm
+++ Overworld/ZSCustomOverworld_Latest.asm
@@ -1695,7 +1695,7 @@ CustomOverworld_LoadSubscreenOverlay_PostInit:
     ; pyramid one by default.
     CPX.w #$00FF : BNE .notFF
         ; The pyramid background.
-        LDX.w #$0096
+        LDX.w #$009F ; #$0096
 
     .notFF
 
