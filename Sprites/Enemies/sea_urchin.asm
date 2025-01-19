8587046d8252716ec3a33d52bc5b0ff2fb18519d
scawful <justin.scofield@outlook.com>
2025-01-19 15:00:05 -0500

    sea urchin force prize drop

--- Sprites/Enemies/sea_urchin.asm
+++ Sprites/Enemies/sea_urchin.asm
@@ -80,11 +80,9 @@ Sprite_SeaUrchin_Main:
   {
     LDA.b #$06 : STA.w SprState, X
     LDA.b #$0A : STA.w SprTimerA, X
-
     STZ.w SprPrize,X
-
-    LDA.b #$09 ; SFX2.1E
-    JSL $0DBB8A ; SpriteSFX_QueueSFX3WithPan
+    JSL ForcePrizeDrop_long
+    LDA.b #$09 : JSL SpriteSFX_QueueSFX3WithPan
     RTS
   }
 }
